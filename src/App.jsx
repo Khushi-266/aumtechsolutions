b92cf9ed268aa79a3eb777dd0cae7ec69cf99726
Khushi-266 <khushidevi20066@gmail.com>
2025-08-04 16:41:11 +0530

    Fix duplicate Router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import Home from './components/Home'
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Team from './components/Team'
-import Career from './components/Career'
-import Contact from './components/Contact'
-import Services from './components/Services'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Team from './components/Team';
+import Career from './components/Career';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Router>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/team' element={<Team />} />
-        <Route path='/career' element={<Career />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/services' element={<Services />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
-      </Router>
       <hr />
-<Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
